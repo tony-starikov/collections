e6963c7d8248af3c1f4ad47ce2e02ee516012fd8
Tony Starikov <tony.starikov.1992@gmail.com>
2023-08-28 00:05:32 +0300

    feat: added user seeder and edited readme, added installation steps

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CollectionSeeder::class,
             ContributorSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
